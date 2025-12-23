d3bb3240dd9788b4c140d097fe09640b02b96687
Dosu333 <vladamir1865@gmail.com>
2025-12-23 12:35:15 +0100

    Add IsAuthenticatedBrandSystem permission class to restrict access to authenticated Brand entities for sensitive lookups

---
+++ app/licenses/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+
+class IsAuthenticatedBrandSystem(permissions.BasePermission):
+    """
+    Strictly limits access to authenticated Brand entities.
+    Used for sensitive cross-brand lookups.
+    """
+    def has_permission(self, request, view):
+        # request.user is populated as a Brand object by our custom Auth class
+        from .models import Brand
+        return isinstance(request.user, Brand)
