f3dcec8fab5c53e1155dd17f1bfa210892600147
Dosu333 <vladamir1865@gmail.com>
2025-12-23 16:40:32 +0100

    Add unique slug generation to Product model

--- app/licenses/models.py
+++ app/licenses/models.py
@@ -55,6 +55,21 @@ class Product(BaseModel):
     description = models.TextField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
+    def generate_unique_slug(self):
+        base_slug = slugify(self.name)
+        slug = base_slug
+        counter = 1
+
+        while Product.objects.filter(slug=slug).exclude(id=self.id).exists():
+            slug = f"{base_slug}-{counter}"
+            counter += 1
+        return slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.generate_unique_slug()
+        super().save(*args, **kwargs)
+
     def __str__(self):
         return self.name
 
