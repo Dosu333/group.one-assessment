e8f1f4cc1a94147d69ccbacad6d1215e645c21f3
Dosu333 <vladamir1865@gmail.com>
2025-12-23 12:37:46 +0100

    Register LicenseKey, License, Brand, Product, and Activation models in the admin site

--- app/licenses/admin.py
+++ app/licenses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import LicenseKey, License, Brand, Product, Activation
 
-# Register your models here.
+
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(LicenseKey)
+admin.site.register(License)
+admin.site.register(Activation)
